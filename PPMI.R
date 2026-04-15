3e78a69521d9228982b5f62939ac416b9f0dd99f
Konstantinos Perperidis <k.a.perperidis@gmail.com>
2026-04-15 19:33:46 +0300

    prep to re-do PPMI analysis w. outlier removal + BMIQ

--- PPMI.R
+++ PPMI.R
@@ -4,69 +4,69 @@ library(dplyr)
 
 data_folder <- "ppmi"
 
-# demographics <- read.csv("./ppmi/Demographics_24Nov2025.csv")
+demographics <- read.csv("./ppmi/Demographics_24Nov2025.csv")
 
-# participant_status <- read.csv("./ppmi/Participant_Status_24Nov2025.csv")
+participant_status <- read.csv("./ppmi/Participant_Status_24Nov2025.csv")
 
-# participant_status <- participant_status %>% filter(!ENROLL_STATUS %in% c("Excluded", "Declined", "Screen_failed"))
+participant_status <- participant_status %>% filter(!ENROLL_STATUS %in% c("Excluded", "Declined", "Screen_failed"))
 
-# patient_meta <- merge(
-#   x = demographics[, c("PATNO", "SEX")],
-#   y = participant_status[, c("ENROLL_STATUS", "PATNO", "COHORT", "ENROLL_AGE", "ENRLPINK1", "ENRLPRKN", "ENRLSRDC", "ENRLNORM", "ENRLOTHGV", "ENRLHPSM", "ENRLLRRK2", "ENRLRBD", "ENRLSNCA", "ENRLGBA")],
-#   by.x = "PATNO",
-#   by.y = "PATNO",
-#   all.x = FALSE
-# )
+patient_meta <- merge(
+  x = demographics[, c("PATNO", "SEX")],
+  y = participant_status[, c("ENROLL_STATUS", "PATNO", "COHORT", "ENROLL_AGE", "ENRLPINK1", "ENRLPRKN", "ENRLSRDC", "ENRLNORM", "ENRLOTHGV", "ENRLHPSM", "ENRLLRRK2", "ENRLRBD", "ENRLSNCA", "ENRLGBA")],
+  by.x = "PATNO",
+  by.y = "PATNO",
+  all.x = FALSE
+)
 
-# ppmi_meth_120_txt <- read.delim("./ppmi/Project120_IDATS_n524final_toLONI_030718/PPMI_Meth_n524_for_LONI030718.txt", header = T, sep = "\t")
+ppmi_meth_120_txt <- read.delim("./ppmi/Project120_IDATS_n524final_toLONI_030718/PPMI_Meth_n524_for_LONI030718.txt", header = T, sep = "\t")
 
-# ppmi_meth_120_meta <- merge(
-#   x = patient_meta,
-#   y = ppmi_meth_120_txt,
-#   by.x = "PATNO",
-#   by.y = "PATNO",
-#   all.x = FALSE
-# )
+ppmi_meth_120_meta <- merge(
+  x = patient_meta,
+  y = ppmi_meth_120_txt,
+  by.x = "PATNO",
+  by.y = "PATNO",
+  all.x = FALSE
+)
 
-# ppmi_meth_120_meta$Basename <- paste0(ppmi_meth_120_meta$Sentrix.ID, "_", ppmi_meth_120_meta$Sentrix.Position)
-# ppmi_meth_120_meta$Sample_Name <- paste0(ppmi_meth_120_meta$PATNO, "_", ppmi_meth_120_meta$Sentrix.ID, "_", ppmi_meth_120_meta$Sentrix.Position)
-# ppmi_meth_120_meta$Sample_Group <- ppmi_meth_120_meta$COHORT
-# ppmi_meth_120_meta <- ppmi_meth_120_meta %>% dplyr::rename(Sentrix_Position = Sentrix.Position, Sentrix_ID = Sentrix.ID)
+ppmi_meth_120_meta$Basename <- paste0(ppmi_meth_120_meta$Sentrix.ID, "_", ppmi_meth_120_meta$Sentrix.Position)
+ppmi_meth_120_meta$Sample_Name <- paste0(ppmi_meth_120_meta$PATNO, "_", ppmi_meth_120_meta$Sentrix.ID, "_", ppmi_meth_120_meta$Sentrix.Position)
+ppmi_meth_120_meta$Sample_Group <- ppmi_meth_120_meta$COHORT
+ppmi_meth_120_meta <- ppmi_meth_120_meta %>% dplyr::rename(Sentrix_Position = Sentrix.Position, Sentrix_ID = Sentrix.ID)
 
-# # 0 -> F; 1 -> M
-# ppmi_meth_120_meta$SEX <- ifelse(ppmi_meth_120_meta$SEX == 0, "Female", "Male")
+# 0 -> F; 1 -> M
+ppmi_meth_120_meta$SEX <- ifelse(ppmi_meth_120_meta$SEX == 0, "Female", "Male")
 
-# # 1 -> PD; 2 -> Control; 3 -> SWEDD; 4 -> Prodromal
-# ppmi_meth_120_meta <- ppmi_meth_120_meta %>%
-#   mutate(Sample_Group = case_when(
-#     Sample_Group == 1 ~ "PD",
-#     Sample_Group == 2 ~ "Control",
-#     Sample_Group == 3 ~ "SWEDD",
-#     Sample_Group == 4 ~ "Prodromal"
-#   ))
+# 1 -> PD; 2 -> Control; 3 -> SWEDD; 4 -> Prodromal
+ppmi_meth_120_meta <- ppmi_meth_120_meta %>%
+  mutate(Sample_Group = case_when(
+    Sample_Group == 1 ~ "PD",
+    Sample_Group == 2 ~ "Control",
+    Sample_Group == 3 ~ "SWEDD",
+    Sample_Group == 4 ~ "Prodromal"
+  ))
 
-# ppmi_meth_120_meta <- ppmi_meth_120_meta %>%
-#   mutate(
-#     Age_Group =
-#       case_when(
-#         ENROLL_AGE >= 30 & ENROLL_AGE < 50 ~ "30-49",
-#         ENROLL_AGE >= 50 & ENROLL_AGE < 60 ~ "50-59",
-#         ENROLL_AGE >= 60 & ENROLL_AGE < 70 ~ "60-69",
-#         ENROLL_AGE >= 70 & ENROLL_AGE < 80 ~ "70-79",
-#         ENROLL_AGE >= 80 ~ "80+",
-#       )
-#   )
+ppmi_meth_120_meta <- ppmi_meth_120_meta %>%
+  mutate(
+    Age_Group =
+      case_when(
+        ENROLL_AGE >= 30 & ENROLL_AGE < 50 ~ "30-49",
+        ENROLL_AGE >= 50 & ENROLL_AGE < 60 ~ "50-59",
+        ENROLL_AGE >= 60 & ENROLL_AGE < 70 ~ "60-69",
+        ENROLL_AGE >= 70 & ENROLL_AGE < 80 ~ "70-79",
+        ENROLL_AGE >= 80 ~ "80+",
+      )
+  )
 
-# write.csv(ppmi_meth_120_meta, "./ppmi/Project120_IDATS_n524final_toLONI_030718/sample_sheet.csv", row.names = F)
+write.csv(ppmi_meth_120_meta, "./ppmi/Project120_IDATS_n524final_toLONI_030718/sample_sheet.csv", row.names = F)
 
-# rm(list = setdiff(ls(), c("data_folder")))
-# gc(full = TRUE)
+rm(list = setdiff(ls(), c("data_folder")))
+gc(full = TRUE)
 
 project_name <- "ppmi"
-# data_folder <- "ppmi"
+data_folder <- "ppmi"
 
-# targets <- read.metharray.sheet(data_folder, pattern = "sample_sheet")
-# saveRDS(targets, file = file.path(data_folder, "targets.rds"))
+targets <- read.metharray.sheet(data_folder, pattern = "sample_sheet")
+saveRDS(targets, file = file.path(data_folder, "targets.rds"))
 
 source("./meta_vars_mapping.R")
 var_mappings <- meta_vars_mapping(dataset = project_name)
@@ -78,7 +78,7 @@ cohorts <- list(
 project_to_load <- "ppmi_20260403_164555"
 source("./pre_process_eda.R")
 pre_process_eda(
-  project_to_load = project_to_load,
+  project_name = project_name,
   targets = targets,
   data_folder = data_folder,
   project_location = "/root/workspace/methyl-pipe-out",
---
+++ R/outlier_remove_redo_BMIQ.R
@@ -0,0 +1,26 @@
+source("R/apply_BMIQ.R")
+
+outlier_remove_redo_BMIQ <- function(context) {
+    rm(list = ls())
+    gc(full = TRUE)
+
+    pca_filename <- "pca_df_with_outliers.rds"
+    targets_filename <- "targets_remove_mismatch.rds"
+    beta_matrix_filename <- "beta_matrix.rds"
+
+    prog <- .create_progress_manager(4)
+    prog$update(1, "Reading PCA results with outlier information")
+    pca <- readRDS(file.path(context$paths$results, pca_filename))
+
+    prog$update(2, "Reading beta-matrix")
+    beta_matrix <- readRDS(file.path(context$paths$results, beta_matrix_filename))
+
+    prog$update(3, "Removing outliers from beta-matrix")
+    outlier_samples <- rownames(pca)[pca$Is_Outlier]
+    beta_matrix_no_outliers <- beta_matrix[, !colnames(beta_matrix) %in% outlier_samples]
+
+    saveRDS(beta_matrix_no_outliers, file.path(context$paths$results, "beta_matrix_no_outliers.rds"))
+
+    prog$update(4, "Re-running BMIQ normalization without outliers")
+    beta_matrix_no_outliers <- apply_BMIQ(context, "beta_matrix_no_outliers.rds")
+}
