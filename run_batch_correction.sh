ba1d1d12242013828aff49ab2e91d8941aeaaa31
Konstantinos Perperidis <k.a.perperidis@gmail.com>
2026-05-16 00:09:50 +0300

    It's sh... not Rgit add .! :/

--- run_batch_correction.sh
+++ run_batch_correction.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-PROJECT_NAME <- ""
-ROOT_DIR <- ""
-TARGET_DF_LOC <- ""
-M_VALUES_LOC <- ""
-IDAT_FOLDER_LOC <- ""
+PROJECT_NAME="GSE111629"
+ROOT_DIR="/root/workspace/methyl-pipe-out/GSE111629_20260515_083253/"
+TARGET_DF_LOC="processed/targets_remove_mismatch.rds"
+M_VALUES_LOC="results/m_values_bmiq.rds"
+IDAT_FOLDER_LOC="/root/methylation-analysis/GSE111629_RAW"
 
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 FILENAME="${PROJECT_NAME}_${TIMESTAMP}"
