9bab3f1528e9c4f75cd33ec2b166b2f3de5427e7
Konstantinos Perperidis <k.a.perperidis@gmail.com>
2026-05-23 14:49:44 +0300

    prep to re-run cell count analysis

--- GSE145361.R
+++ GSE145361.R
@@ -8,24 +8,26 @@ gc(full = TRUE)
 project_name <- "GSE145361"
 data_folder <- paste0(project_name, "_RAW")
 
-# targets <- read.metharray.sheet(data_folder, pattern = "GPL13534_HumanMethylation450_15017482_v.1.1.csv.gz")
-# gse <- getGEO(project_name, GSEMatrix = TRUE, getGPL = FALSE)
-# pdata <- pData(gse[[1]])
-# all_idat_files <- list.files(data_folder, pattern = "\\.idat\\.gz$", full.names = FALSE)
-# basenames <- unique(gsub("_(Grn|Red).idat.gz", "", all_idat_files))
-# targets <- data.frame(
-#     Sample_Name = gsub("_.*", "", basenames),
-#     Basename = file.path(data_folder, basenames),
-#     stringsAsFactors = FALSE
-# )
+targets <- read.metharray.sheet(data_folder, pattern = "GPL13534_HumanMethylation450_15017482_v.1.1.csv.gz")
+gse <- getGEO(project_name, GSEMatrix = TRUE, getGPL = FALSE)
+pdata <- pData(gse[[1]])
+all_idat_files <- list.files(data_folder, pattern = "\\.idat\\.gz$", full.names = FALSE)
+basenames <- unique(gsub("_(Grn|Red).idat.gz", "", all_idat_files))
+targets <- data.frame(
+    Sample_Name = gsub("_.*", "", basenames),
+    Basename = file.path(data_folder, basenames),
+    stringsAsFactors = FALSE
+)
 
-# targets <- merge(targets, pdata, by.x = "Sample_Name", by.y = "row.names", all.x = TRUE)
-# targets$Sample_Group <- factor(targets$`disease-state:ch1`)
-# targets <- targets %>% mutate(Sample_Group = case_when(Sample_Group == "Parkinson's disease" ~ "PD", Sample_Group == "PD-free control" ~ "Control"))
-# targets$Sample_Group <- as.factor(targets$Sample_Group)
+targets <- merge(targets, pdata, by.x = "Sample_Name", by.y = "row.names", all.x = TRUE)
+targets$Sample_Group <- factor(targets$`disease-state:ch1`)
+targets <- targets %>% mutate(Sample_Group = case_when(Sample_Group == "Parkinson's disease" ~ "PD", Sample_Group == "PD-free control" ~ "Control"))
+targets$Sample_Group <- as.factor(targets$Sample_Group)
 
 # rm(list = setdiff(ls(), c("targets", "project_name", "project_to_load", "data_folder")))
 # gc(full = T)
+
+saveRDS(targets, file.path("/root/workspace/methyl-pipe-out/GSE145361_20260522_110341/raw_data", "targets.rds"))
 
 source("./meta_vars_mapping.R")
 var_mappings <- meta_vars_mapping(dataset = project_name)
