32f1d106dff0851b9ad06a3fe0abd10b9faf625d
Konstantinos Perperidis <k.a.perperidis@gmail.com>
2026-06-06 00:09:30 +0300

    gh action to create img

--- GSE145361.R
+++ GSE145361.R
@@ -8,18 +8,21 @@ gc(full = TRUE)
 project_name <- "GSE145361"
 data_folder <- paste0(project_name, "_RAW")
 
-# targets <- read.metharray.sheet(data_folder, pattern = "GPL13534_HumanMethylation450_15017482_v.1.1.csv.gz")
-# gse <- getGEO(project_name, GSEMatrix = TRUE, getGPL = FALSE)
-# pdata <- pData(gse[[1]])
-# all_idat_files <- list.files(data_folder, pattern = "\\.idat\\.gz$", full.names = FALSE)
-# basenames <- unique(gsub("_(Grn|Red).idat.gz", "", all_idat_files))
-# targets <- data.frame(
-#     Sample_Name = gsub("_.*", "", basenames),
-#     Basename = file.path(data_folder, basenames),
-#     stringsAsFactors = FALSE
-# )
+targets <- read.metharray.sheet(data_folder, pattern = "GPL13534_HumanMethylation450_15017482_v.1.1.csv.gz")
 
-# targets <- merge(targets, pdata, by.x = "Sample_Name", by.y = "row.names", all.x = TRUE)
+gse <- getGEO(project_name, GSEMatrix = TRUE, getGPL = FALSE)
+pdata <- pData(gse[[1]])
+all_idat_files <- list.files(data_folder, pattern = "\\.idat\\.gz$", full.names = FALSE)
+basenames <- unique(gsub("_(Grn|Red).idat.gz", "", all_idat_files))
+targets <- data.frame(
+    Sample_Name = gsub("_.*", "", basenames),
+    Basename = file.path(data_folder, basenames),
+    stringsAsFactors = FALSE
+)
+
+targets <- merge(targets, pdata, by.x = "Sample_Name", by.y = "row.names", all.x = TRUE)
+targets %>% head()
+
 # targets$Sample_Group <- factor(targets$`disease-state:ch1`)
 # targets <- targets %>% mutate(Sample_Group = case_when(Sample_Group == "Parkinson's disease" ~ "PD", Sample_Group == "Control" ~ "Control"))
 # targets$Sample_Group <- as.factor(targets$Sample_Group)
