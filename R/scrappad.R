ab4363b6dcdc414ecac775961f1b16a07bfb1df7
perpk <k.a.perperidis@gmail.com>
2026-06-26 09:56:41 +0000

    scrapbook to debug issues

--- R/scrappad.R
+++ R/scrappad.R
@@ -13,10 +13,51 @@ source("R/intermediate_data_proxy.R")
 project_name <- "GSE111629"
 project_to_load <- NULL
 project_location <- "/root/workspace/methyl-pipe-out"
-var_mapping <- var_mappings
+source("./meta_vars_mapping.R")
+library(IlluminaHumanMethylation450kanno.ilmn12.hg19)
+library(GEOquery)
+library(tidyverse)
+  
+rm(list = ls())
+gc(full = TRUE)
+
+project_name <- "GSE111629"
+project_to_load <- "GSE111629_20260406_185759"
+data_folder <- paste0(project_name, "_RAW")
+
+# targets <- read.metharray.sheet(data_folder, pattern = "GPL13534_HumanMethylation450_15017482_v.1.1.csv.gz")
+gse <- getGEO(project_name, GSEMatrix = TRUE, getGPL = FALSE)
+pdata <- pData(gse[[1]])
+all_idat_files <- list.files(data_folder, pattern = "\\.idat\\.gz$", full.names = FALSE)
+basenames <- unique(gsub("_(Grn|Red).idat.gz", "", all_idat_files))
+targets <- data.frame(
+  Sample_Name = gsub("_.*", "", basenames),
+  Basename = file.path(data_folder, basenames),
+  stringsAsFactors = FALSE
+)
+targets <- merge(targets, pdata, by.x = "Sample_Name", by.y = "row.names", all.x = TRUE)
+targets$Sample_Group <- factor(targets$`disease state:ch1`)
+targets <- targets %>% mutate(Sample_Group = case_when(Sample_Group == "Parkinson's disease (PD)" ~ "PD", Sample_Group == "PD-free control" ~ "Control"))
+targets$Sample_Group <- as.factor(targets$Sample_Group)
+
+rm(list = setdiff(ls(), c("targets", "project_name", "project_to_load", "data_folder")))
+gc(full = T)
+
+source("./meta_vars_mapping.R")
+var_mappings <- meta_vars_mapping(dataset = project_name)
+
+cohorts <- list(
+  PD_vs_Control = c("PD", "Control")
+)
+
+
+var_mapping <- meta_vars_mapping(dataset = project_name)
 platform <- "450k"
 qc_threshold <- 10.5
-
+cohorts <- list(
+  PD_vs_Control = c("PD", "Control")
+)
+platform = "450k"
 if (is.null(platform)) {
   stop("Platform must be specified as '450K' or 'EPIC'")
 }
