f4f29dd774bcd33fe7812fce79336fe526af5fd5
Konstantinos Perperidis <k.a.perperidis@gmail.com>
2026-07-21 10:09:25 +0300

    lint fixes and sync disk ops

---
+++ .Rprofile
@@ -0,0 +1,2 @@
+# Tells the R language server and styler to format using 4 spaces
+options(styler.addins_style_transformer = "styler::tidyverse_style(indent_by = 4)")
--- R/intermediate_data_proxy.R
+++ R/intermediate_data_proxy.R
@@ -1,49 +1,57 @@
-
-intermediate_data_proxy <- function(pipeline_function, project_context, ...) {
+intermediate_data_proxy <- function(pipeline_function, auto_clean = FALSE, project_context, ...) {
     library(mirai)
+    source("R/project_context.R")
 
     results <- pipeline_function(project_context, ...)
 
-    ## In case of disk-only, the memory must be cleaned up, 
-    ### since presumably the user does not want to keep the 
-    ### results in memory. In case of disk-and-memory, the 
-    ### results are kept in memory, but also saved to disk 
-    ### asynchronously. In case of memory-only, the results 
+    ## In case of disk-only, the memory must be cleaned up,
+    ### since presumably the user does not want to keep the
+    ### results in memory. In case of disk-and-memory, the
+    ### results are kept in memory, but also saved to disk
+    ### asynchronously. In case of memory-only, the results
     ### are kept in memory and not saved to disk.
     if (project_context$mode == results_mode()$disk_only) {
         print("Disk only mode: Saving results to disk (sync).")
         updated_results <- list()
-        
+
         updated_results <- lapply(results, function(result) {
+            saveRDS(result@object, file = result@filename)
+            if (auto_clean) {
+                rm(list = deparse(substitute(result@object)))
+            }
             if (is(result, "ResultsContainer")) {
-                future <- mirai({
-                    cat(paste("Saving", f, "to disk synchronously.\n"))
-                    saveRDS(o, file = f)
-                    return(f)
-                }, o = result@object, f = result@filename)
-                
-                return(new("ResultsContainer", 
+                return(new("ResultsContainer",
                     filename = result@filename,
                     object = result@object,
-                    future = future))
+                    future = NULL
+                ))
+            } else {
+                return(result)
             }
-            return(result)
         })
+        gc(full = TRUE)
         return(updated_results)
     }
     if (project_context$mode == results_mode()$disk_and_memory) {
         print("Disk and memory mode: Saving results to disk (async) and keeping them in memory.")
         updated_results <- lapply(results, function(result) {
             if (is(result, "ResultsContainer")) {
-                future <- mirai({
-                    cat(paste("Saving", f, "to disk asynchronously.\n"))
-                    saveRDS(o, file = f)
-                    return(f)
-                }, o = result@object, f = result@filename)
-                updated_result <- new("ResultsContainer", 
+                future <- mirai(
+                    {
+                        cat(paste("Saving", f, "to disk asynchronously.\n"))
+                        saveRDS(o, file = f)
+                        return(f)
+                    },
+                    o = result@object,
+                    f = result@filename
+                )
+                return(new("ResultsContainer",
                     filename = result@filename,
                     object = result@object,
-                    future = future)
+                    future = future
+                ))
+            } else {
+                return(result)
             }
         })
         return(updated_results)
