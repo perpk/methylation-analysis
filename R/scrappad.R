959cb4fd31bfd5e981c5ae379489ee0b4ac40412
Konstantinos Perperidis <k.a.perperidis@gmail.com>
2026-07-31 20:50:08 +0300

    prep to re-do BMIQ normalization

--- R/scrappad.R
+++ R/scrappad.R
@@ -17,174 +17,100 @@ source("R/project_context.R")
 source("R/intermediate_data_proxy.R")
 source("R/results_container.R")
 
-project_to_load <- "GSE111629_20260722_083339"
-project_location <- "/root/workspace/methyl-pipe-out"
-platform <- "450k"
-data_folder <- "GSE111629_RAW"
+# wget https://drive.usercontent.google.com/download?id=1D9PyaHhZNBB2-DW80ZaAgcIlomfgl6lB&export=download&authuser=0&confirm=t&uuid=9608423f-4d31-4d7f-aa4f-8ee7efe1e455&at=AFYLz4Pmx-cmnG7juuQM_dl3Y9BE:1785519150140 -O targets_s_mismatch_cells_scandate.rds
+# wget https://drive.usercontent.google.com/download?id=1045WmyC8psedUMwgmtEy1o-_YOZJGyBk&export=download&authuser=0&confirm=t&uuid=bef8834a-7bc2-4675-8dca-ad6eea651173&at=AFYLz4PDSABhzUFn-lDr-54S7O5D:1785519212713 -O beta_matrix.rds
+# wget https://drive.usercontent.google.com/download?id=1LTlKTziuwnYND3ULSr4TtYex9GcVTwQO&export=download&authuser=0&confirm=t&uuid=216af4b7-b769-42ad-976a-d4ace364b2a1&at=AFYLz4MM5Gf62YfvUs1gYoxixHWB:1785519395943 -O beta_matrix_bmiq.rds
+# wget https://drive.usercontent.google.com/download?id=1pKEXWGd-WtXHLQc8oHGSSKQpm_kV4D4E&export=download&authuser=0&confirm=t&uuid=89cca26a-db7d-4454-abb2-4a7edb022dfc&at=AFYLz4PiJ0lNZ9F0sLFpaPhXT8mU:1785519465129 -O m_values_bmiq.rds
+# wget https://drive.usercontent.google.com/download?id=1PIaVMFefCEJxiW2dVaCFoMxRLFkBQCWE&export=download&authuser=0&confirm=t&uuid=535c8b79-860e-41c9-b722-b1828a528727&at=AFYLz4ORcayXZgTrw9A2li1-sDGs:1785519568230 -O pca_df_with_outliers.rds
 
-cohorts <- list(
-  PD_vs_Control = c("PD", "Control")
-)
-mode <- results_mode()$memory_only
+beta_matrix <- readRDS("/root/data/beta_matrix.rds")
+beta_matrix_bmiq <- readRDS("/root/data/beta_matrix_bmiq.rds")
+m_values_bmiq <- readRDS("/root/data/m_values_bmiq.rds")
 
-project_context <- .load_methylation_project(project_location, project_to_load, platform = platform, cohorts = cohorts)
-project_context$mode <- mode
-project_context$mode
+targets <- readRDS("/root/data/targets_s_mismatch_cells_scandate.rds")
 
-beta_before <- readRDS("/root/workspace/methyl-pipe-out/GSE111629_20260722_083339/results/beta_matrix.rds")
-beta_bmiq <- readRDS("/root/workspace/methyl-pipe-out/GSE111629_20260722_083339/results/beta_matrix_bmiq.rds")
+pca_df_with_outliers <- readRDS("/root/data/pca_df_with_outliers.rds")
 
-source("R/apply_BMIQ.R")
-plot_BMIQ(
-  context = project_context,
-  beta_before = beta_before,
-  beta_after = beta_bmiq
-)
+outlier_samples <- rownames(pca_df_with_outliers)[pca_df_with_outliers$Is_Outlier]
+beta_matrix_no_outliers <- beta_matrix[, !colnames(beta_matrix) %in% outlier_samples]
 
-# Principal Component Analysis
-source("R/principal_component_analysis.R")
-source("./meta_vars_mapping.R")
-var_mappings <- meta_vars_mapping(dataset = "GSE111629")
-col_map <- list()
-col_map[["Sample_Group"]] <- "Sample_Group"
-col_map[["Gender"]] <- var_mappings$gender_var
-col_map[["Age"]] <- var_mappings$age_var
-keys <- c("Sample_Group", "Gender", "Age")
+beta_bmiq <- matrix(NA, nrow = nrow(beta_matrix_no_outliers), ncol = ncol(beta_matrix_no_outliers))
+rownames(beta_bmiq) <- rownames(beta_matrix_no_outliers)
+colnames(beta_bmiq) <- colnames(beta_matrix_no_outliers)
 
-m_values_bmiq <- readRDS(file.path(project_context$paths$results, "m_values_bmiq.rds"))
-targets <- readRDS(file.path(project_context$paths$processed, "targets_remove_mismatch.rds"))
+.get_probe_design_vector <- function(probe_names, platform) {
+  # Returns: 1 for Type I probes, 2 for Type II probes
+  anno <- getAnnotation(platform)
 
-m_bmiq_container <- new("ResultsContainer", filename = file.path(project_context$paths$results, "m_values_bmiq.rds"), object = m_values_bmiq, future = NULL)
-targets_container <- new("ResultsContainer", filename = file.path(project_context$paths$processed, "targets_remove_mismatch.rds"), object = targets, future = NULL)
+  # Match probes to annotation (handle missing probes)
+  matched_probes <- intersect(probe_names, rownames(anno))
 
-bmiq_res <- list(
-  m_bmiq_container = m_bmiq_container
-)
+  if (length(matched_probes) < length(probe_names)) {
+    warning(paste(
+      length(probe_names) - length(matched_probes),
+      "probes not found in annotation"
+    ))
+  }
 
-res_bio_gender_mismatch <- list(
-  targets_container = targets_container
-)
+  # Get probe types
+  probe_types <- rep(NA, length(probe_names))
+  names(probe_types) <- probe_names
 
-res_pca <- intermediate_data_proxy(
-  principal_component_analysis,
-  auto_clean = FALSE,
-  project_context,
-  m_values = bmiq_res$m_bmiq_container@object,
-  m_values_filename = bmiq_res$m_bmiq_container@filename,
-  targets = res_bio_gender_mismatch$targets_container@object,
-  targets_filename = res_bio_gender_mismatch$targets_container@filename,
-  col_maps = col_map,
-  keys = keys,
-  npc = 5
-)
+  # Type I = 1, Type II = 2 (BMIQ convention)
+  probe_types[matched_probes] <- ifelse(anno[matched_probes, "Type"] == "I", 1, 2)
 
-saveRDS(res_pca$pca_df_container@object, file.path(project_context$paths$results, "pca_df.rds"))
+  # Check for NAs
+  if (any(is.na(probe_types))) {
+    warning("Some probes could not be classified. Using alternative method...")
 
-source("R/plot_PCA.R")
-pca_vars <- c("Sample_Group" = "By Diagnosis", "Gender" = "By Biological Gender", "Age" = "By Age")
-convert_f <- function(df) {
-  (transform(df, Age = as.numeric(Age)))
-}
-plot_PCA(
-  context = project_context,
-  pca_df = res_pca$pca_df_container@object,
-  pca_vars = pca_vars,
-  convert_fun = convert_f,
-  continuously_scaled = c("Age"),
-  "pca_plot_",
-  pairplot_color_by = "Age"
-)
+    # Alternative: Use probe name patterns
+    # Type I: typically start with cg and have specific Infinium I design
+    # Type II: typically start with ch or some cg with Infinium II design
+    # This is less reliable but works as fallback
+    na_probes <- probe_names[is.na(probe_types)]
+    probe_types[na_probes] <- ifelse(grepl("^ch", na_probes), 2, 1)
+  }
 
-res_pca$pca_df_container@object %>% head()
-
-beta_bmiq <- readRDS(file.path(project_context$paths$results, "beta_matrix_bmiq.rds"))
-beta_bmiq_container <- new("ResultsContainer", filename = file.path(project_context$paths$results, "beta_matrix_bmiq.rds"), object = beta_bmiq, future = NULL)
-bmiq_res <- list(
-  beta_bmiq_container = beta_bmiq_container,
-  m_bmiq_container = m_bmiq_container
-)
-
-source("R/outlier_analysis.R")
-res_outlier <- intermediate_data_proxy(
-  outlier_analysis,
-  auto_clean = FALSE,
-  project_context,
-  pca = res_pca$pca_df_container@object,
-  pca_filename = res_pca$pca_df_container@filename,
-  targets = res_bio_gender_mismatch$targets_container@object,
-  targets_filename = res_bio_gender_mismatch$targets_container@filename,
-  beta_bmiq = bmiq_res$beta_bmiq_container@object,
-  beta_bmiq_filename = bmiq_res$beta_bmiq_container@filename
-)
-
-saveRDS(res_outlier$pca_outliers_container@object, res_outlier$pca_outliers_container@filename)
-
-source("R/outlier_remove_redo_BMIQ.R")
-outlier_removed_bmiq_res <- outlier_remove_redo_BMIQ(
-  context = project_context,
-  pca = res_outlier$pca_outliers_container@object,
-  beta_matrix = bmiq_res$beta_bmiq_container@object,
-  pca_filename = res_outlier$pca_outliers_container@filename,
-  beta_matrix_filename = bmiq_res$beta_bmiq_container@filename
-)
-
-beta_matrix <- outlier_removed_bmiq_res$beta_bmiq_container@object
-targets <- res_bio_gender_mismatch$targets_container@object
-
-library(dplyr)
-library(stringr)
-
-rownames(targets) <- targets$Basename %>% str_remove(paste0("ppmi/", "Project120_IDATS_n524final_toLONI_030718", "/"))
-rownames(targets) <- targets$Basename %>% str_remove("ppmi")
-
-rownames(targets) %>% head()
-
-pd_samples <- rownames(targets[targets$Sample_Group == "PD", ])
-hc_samples <- rownames(targets[targets$Sample_Group == "Control", ])
-
-colnames_pd <- which(colnames(beta_matrix) %in% pd_samples)
-colnames_hc <- which(colnames(beta_matrix) %in% hc_samples)
-
-mean_beta_pd <- rowMeans(beta_matrix[, colnames_pd], na.rm = TRUE)
-mean_beta_hc <- rowMeans(beta_matrix[, colnames_hc], na.rm = TRUE)
-delta_beta <- mean_beta_pd - mean_beta_hc
-
-beta_means <- data.frame(
-  mean_beta_pd,
-  mean_beta_hc,
-  delta_beta
-)
-
-write.csv(beta_means, file.path(project_context$paths$results, "beta_means.csv"))
-
-# --
-
-rg_set <- readRDS(file.path(project_context$paths$raw_data, "rg_set.rds"))
-targets <- readRDS(file.path(project_context$paths$processed, "targets.rds"))
-
-
-source("R/cell_cnt_estimate.R")
-res_cell_cnt_estimate <- intermediate_data_proxy(
-  cell_cnt_estimate,
-  auto_clean = FALSE,
-  project_context,
-  rg_set = rg_set,
-  targets = targets
-)
-
-if (project_context$mode == results_mode()$memory_only) {
-  print(paste("Saving targets after cell count estimation to", file.path(project_context$paths$processed, "targets_after_cell_count_estimation.rds")))
-  saveRDS(res_cell_cnt_estimate$targets_container@object, file.path(project_context$paths$processed, "targets_after_cell_count_estimation.rds"))
+  return(probe_types)
 }
 
-# Plot Cell proportion per cohort and write results to files for each cell type
-source("R/plot_cell_proportions.R")
-plot_cell_proportions(
-  context = project_context,
-  targets = res_cell_cnt_estimate$targets_container@object,
-  cell_types = getCellTypesForPlatform(project_context$platform)
-)
-getCellTypesForPlatform(project_context$platform)
-project_context$platform
+design.v <- .get_probe_design_vector(rownames(beta_matrix_no_outliers), platform)
 
-res_cell_cnt_estimate$targets_container@object %>% head()
+print(paste("Type I probes:", sum(design.v == 1)))
+print(paste("Type II probes:", sum(design.v == 2)))
+
+beta_bmiq <- matrix(NA, nrow = nrow(beta_matrix_no_outliers), ncol = ncol(beta_matrix_no_outliers))
+rownames(beta_bmiq) <- rownames(beta_matrix_no_outliers)
+colnames(beta_bmiq) <- colnames(beta_matrix_no_outliers)
+
+
+for (i in 1:ncol(beta_matrix_no_outliers)) {
+  print(paste("Processing sample", i, "of", ncol(beta_matrix_no_outliers)))
+
+  # Extract beta values for this sample
+  beta.v <- beta_matrix_no_outliers[, i]
+
+  # Apply BMIQ to this sample
+  tryCatch(
+    {
+      bmiq_result <- BMIQ(
+        beta.v = beta.v,
+        design.v = design.v,
+        nfit = 10000,
+        plots = FALSE,
+        pri = TRUE
+      )
+      print(bmiq_result$nbeta[1:10])
+
+      # Store the normalized values
+      beta_bmiq[, i] <- bmiq_result$nbeta
+    },
+    error = function(e) {
+      warning(paste("BMIQ failed for sample", colnames(beta_matrix_no_outliers)[i], ":", e$message))
+      beta_bmiq[, i] <- beta.v # Keep original values if BMIQ fails
+    }
+  )
+}
+
+m_bmiq <- log2(beta_bmiq / (1 - beta_bmiq))
+m_bmiq[is.infinite(m_bmiq)] <- NA
