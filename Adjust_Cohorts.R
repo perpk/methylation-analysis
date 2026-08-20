fb8453a822bdbffc092226b31b7926e657103032
Konstantinos Perperidis <k.a.perperidis@gmail.com>
2026-08-20 22:03:54 +0300

    Combat adjust according to PEG1 for ML (for starters)

---
+++ Adjust_Cohorts.R
@@ -0,0 +1,46 @@
+rm(list = ls())
+gc(full = TRUE)
+
+library(tidyverse)
+
+m_values_combined <- readRDS("/workspace/results/consolidated_m_values.rds")
+targets_combined <- readRDS("/workspace/results/consolidated_targets.rds")
+
+targets_combined$Cohort <- as.factor(targets_combined$Cohort)
+targets_combined$Sample_Group <- as.factor(targets_combined$Sample_Group)
+targets_combined$Sex <- as.factor(targets_combined$Sex)
+
+design <- model.matrix(~ Sample_Group + Sex + CD4T + CD8T + NK + Bcell + Mono + Gran, data = targets_combined)
+
+library(sva)
+
+m_values_combat_corr <- ComBat(
+    dat = m_values_combined,
+    batch = targets_combined$Cohort,
+    mod = design,
+    par.prior = TRUE,
+    ref.batch = "PEG1",
+    prior.plots = FALSE
+)
+
+pca_prev <- prcomp(t(m_values_combined), center = TRUE, scale. = TRUE)
+pca_df_before <- data.frame(pca_prev$x[, 1:2], Cohort = targets_combined$Cohort, Group = targets_combined$Sample_Group)
+
+ggplot(pca_df_before, aes(x = PC1, y = PC2, color = Cohort)) +
+    geom_point(alpha = 0.7) +
+    theme_minimal() +
+    labs(
+        title = "PCA of M-Values Before ComBat Harmonization"
+    )
+
+pca_after <- prcomp(t(m_values_combat_corr), center = TRUE, scale. = TRUE)
+pca_df_after <- data.frame(pca_after$x[, 1:2], Cohort = targets_combined$Cohort, Group = targets_combined$Sample_Group)
+
+ggplot(pca_df_after, aes(x = PC1, y = PC2, color = Cohort)) +
+    geom_point(alpha = 0.7) +
+    theme_minimal() +
+    labs(
+        title = "PCA of M-Values After ComBat Harmonization"
+    )
+
+saveRDS(m_values_combat_corr, "/workspace/results/combat_corrected_m_values.rds")
