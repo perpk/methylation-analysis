0a8cd1b15fd3242de9522ae1e166fc21d3fa9302
Konstantinos Perperidis <k.a.perperidis@gmail.com>
2026-09-17 23:36:08 +0300

    debug

--- python/batch/assess_feature_importances.py
+++ python/batch/assess_feature_importances.py
@@ -149,7 +149,7 @@ for cohort, path in cohorts.items():
         
         # 2. Grab the first 5 probe IDs from the graph topology 
         # (Adjust ['probes'] to whatever key your build_chromosome_topologies uses to store the probe list)
-        print(chr_topologies.head())
+        print(chr_topologies)
         graph_top_5 = chr_topologies[chr_name]['probes'][:5] 
         
         # 3. Print the comparison
