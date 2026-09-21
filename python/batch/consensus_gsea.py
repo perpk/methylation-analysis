e53c719365f325087d8c241ca19fe7652bcda4f5
Konstantinos Perperidis <k.a.perperidis@gmail.com>
2026-09-21 20:57:47 +0300

    take a nap between gsea api requests

--- python/batch/consensus_gsea.py
+++ python/batch/consensus_gsea.py
@@ -1,3 +1,5 @@
+from asyncio import sleep
+
 import pandas as pd
 import gseapy as gp
 import matplotlib.pyplot as plt
@@ -66,6 +68,7 @@ def main():
             organism='human', 
             outdir=None
         )
+        sleep(10)  # To avoid hitting API rate limits
         results_df = enrichment.results
         fdr_sig = results_df[results_df['Adjusted P-value'] < 0.05]
         print(f"{name}: Found {len(fdr_sig)} strictly significant terms (FDR < 0.05)")
